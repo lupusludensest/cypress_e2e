d7b7def80310bec6d9862edd6de3b313854d1292
LupusLudensEst <gurovvic@gmail.com>
2021-12-14 23:27:40 -0500

    dt 14 dec 2021

--- cypress/integration/tSsamples/updateAgentNeuroDev-spec.ts
+++ cypress/integration/tSsamples/updateAgentNeuroDev-spec.ts
@@ -2,9 +2,12 @@ let url = "https://cms-dev-v3.neuro.net";
 
 describe('To get token', () => {
 
+    console.log(url);
+
     const login = 'test_cypress@neuro.net';
     const password = 'test_cypresS6';
     let token = '';
+    let agent_uuid = '';
     console.log(url);
 
     it('To get token', () => {
@@ -23,6 +26,8 @@ describe('To get token', () => {
                 cy.log(JSON.stringify(responce));
                 cy.log(responce.body.token);
                 token = responce.body.token;
+                cy.log(responce.body.agent_uuid);
+                agent_uuid = responce.body.agent_uuid;
             });
     })
 
@@ -45,11 +50,13 @@ describe('To get token', () => {
 
                 // Expected
             }).then((res) => {
+
                 cy.log(JSON.stringify(res))
                 expect(res.status).to.eq(200)
                 expect(res.body).has.property('recall_count', 12345678)
                 expect(res.body).has.property('language', 'en-US')
                 expect(res.body).has.property('company_uuid', '988aef0a-7467-48e4-a4a9-2193da260718')
+                expect(res.body).has.property('flag', 'test_one')
             })
         })
     })
